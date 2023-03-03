e6cd3e30552e4a9d87cce8f00ebfaf16fb74b1e3
Lars Volkers <larsvolkers@hotmail.nl>
2023-03-03 15:28:13 +0100

    Api setup

---
+++ SwaggerDocumentsDemo.Api/Data/InMemoryDatabase.cs
@@ -0,0 +1,8 @@
+﻿using SwaggerDocumentsDemo.Api.Models;
+
+namespace SwaggerDocumentsDemo.Api.Data;
+
+public class InMemoryDatabase
+{
+    public static readonly List<WeatherModel> Weather = new();
+}
---
+++ SwaggerDocumentsDemo.Api/Endpoints/InteralPublicWeatherEndpoints.cs
@@ -0,0 +1,68 @@
+﻿using SwaggerDocumentsDemo.Api.Data;
+using SwaggerDocumentsDemo.Api.Models;
+
+namespace SwaggerDocumentsDemo.Api.Endpoints;
+
+public static class InteralPublicWeatherEndpoints
+{
+
+    private static readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static IEndpointRouteBuilder AddInternalWeatherApi(this IEndpointRouteBuilder app)
+    {
+        var weatherApi = app
+            .MapGroup("internal/weather")
+            .WithTags("Internal weather endpoints")
+            .WithOpenApi();
+
+        weatherApi.MapPost("/", AddWeather)
+            .WithName("PostWeather");
+
+        weatherApi.MapPut("/", UpdateWeather)
+            .WithName("PutWeather");
+
+        weatherApi.MapPost("/forecast", Forecast)
+        .WithName("ForecastWeather");
+
+
+        return app;
+    }
+
+    private static WeatherModel AddWeather(WeatherModel weather)
+    {
+        InMemoryDatabase.Weather.Add(weather);
+        return weather;
+    }
+
+    private static WeatherModel Forecast(DateOnly date)
+    {
+        var forecast = new WeatherModel
+            (
+                date,
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            );
+        InMemoryDatabase.Weather.Add(forecast);
+        return forecast;
+    }
+
+    private static WeatherModel UpdateWeather(WeatherModel weather)
+    {
+
+        var existingWeather = InMemoryDatabase.Weather.FirstOrDefault(x => x.Date == weather.Date);
+        if (existingWeather != null)
+        {
+            existingWeather.Summary = weather.Summary;
+            existingWeather.TemperatureC = weather.TemperatureC;
+        }
+        else
+        {
+            InMemoryDatabase.Weather.Add(weather);
+        }
+        return weather;
+
+    }
+}
