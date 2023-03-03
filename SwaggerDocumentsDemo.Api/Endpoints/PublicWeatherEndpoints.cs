f7c2eb4a0773053a5e8285676b975f5d315029cd
Lars Volkers <larsvolkers@hotmail.nl>
2023-03-03 15:28:51 +0100

    Api setup

---
+++ SwaggerDocumentsDemo.Api/Endpoints/PublicWeatherEndpoints.cs
@@ -0,0 +1,28 @@
+﻿using SwaggerDocumentsDemo.Api.Data;
+
+namespace SwaggerDocumentsDemo.Api.Endpoints;
+
+public static class PublicWeatherEndpoints
+{
+    private static readonly string[] summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    public static IEndpointRouteBuilder AddPublicWeatherApi(this IEndpointRouteBuilder app)
+    {
+        var weatherApi = app
+            .MapGroup("weather")
+            .WithTags("Public weather endpoints")
+            .WithOpenApi();
+
+        weatherApi.MapGet("/", () => InMemoryDatabase.Weather)
+            .WithName("GetWeather");
+
+        weatherApi.MapGet("/{date}", (DateOnly date) => InMemoryDatabase.Weather.FirstOrDefault(weather => weather.Date == date))
+            .WithName("GetWeatherByDate");
+
+        return app;
+    }
+}
+
---
+++ SwaggerDocumentsDemo.Api/Models/WeatherModel.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace SwaggerDocumentsDemo.Api.Models;
+
+public class WeatherModel
+{
+    public DateOnly Date { get; set; }
+    public int TemperatureC { get; set; }
+    public string? Summary { get; set; }
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public WeatherModel(DateOnly date, int temperatureC, string? summary)
+    {
+        Date = date;
+        TemperatureC = temperatureC;
+        Summary = summary;
+    }
+}
